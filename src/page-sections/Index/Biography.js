61b7bad4d2a4a7df108fff086bf61167fc1c62db
Navid <navid.naseri.079@gmail.com>
2022-01-30 13:14:05 +0330

    Biography component added

---
+++ src/page-sections/Index/Biography.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Biography.css';
+
+const Biography = ({ title, imageUrl, flipped, children }) => {
+  return (
+    <div className={`biography ${flipped ? 'biography--flipped' : ''}`}>
+      <div className='biography__image-container'>
+        <img className='biography__image' src={imageUrl}></img>
+      </div>
+      <div className='biography__body'>
+        <h2 className='biography__title'>{title}</h2>
+        <p className='biography__text'>{children}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Biography;
